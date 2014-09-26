34614b9c34dca9587309fa7c385f8427ec33559b
J Bae <johnhjbae@gmail.com>
2014-09-25 21:26:05 -0400

    This is so cool

---
+++ src/com/john/models/People.java
@@ -0,0 +1,40 @@
+package com.john.models;
+
+public class People {
+	private String name;
+	private String gender;
+	private int age;
+	private String job;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	
+	@Override
+	public String toString() {
+		if(name == null ) name = "John Doe";
+		return "Hi My name is "+getName();
+	}
+	
+	
+}
---
+++ src/com/john/test/Main.java
@@ -0,0 +1,14 @@
+package com.john.test;
+
+import com.john.models.People;
+
+public class Main {
+
+	public static void main(String[] args) {
+		People p = new People();
+		p.setName("John Bae");
+		p.setAge(44);
+		p.setJob("Computer Programmer");
+	}
+
+}
